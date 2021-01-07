417f105dae9a164022113d85b2b957af78088533
Mahalingam Sundararaj <mathans1695@gmail.com>
2021-01-07 22:09:49 +0530

    create vehicles and orbits

---
+++ initialize.py
@@ -0,0 +1,11 @@
+from vehicle import Vehicle
+from orbit import Orbit
+
+# create three instance of vehicle
+bike = Vehicle("bike", 10, 2, ["sunny", "windy"])
+tuktuk = Vehicle("tuk tuk", 12, 1, ["sunny", "rainy"])
+car = Vehicle("car", 20, 3, ["sunny", "rainy", "windy"])
+
+# create two instance of orbit
+orbit1 = Orbit("orbit1", 18, 20)
+orbit2 = Orbit("orbit2", 20, 10)
